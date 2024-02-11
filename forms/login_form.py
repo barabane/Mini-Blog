eb21d6b027d233cfdf2a0b196014949c093f765a
barabane <barabayne@gmail.com>
2024-02-11 23:14:14 +0500

    Login/Sign-up verification forms

--- forms/login_form.py
+++ forms/login_form.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms.validators import DataRequired, InputRequired, Email, Length
 
 
 class LoginForm(FlaskForm):
-    email = EmailField('Email', validators=[DataRequired(), InputRequired(message="Введите Email")])
-    password = PasswordField('Пароль', validators=[DataRequired(), InputRequired(message="Введите пароль")])
+    email = EmailField('Email',
+                       validators=[DataRequired(message="Введите Email"), InputRequired(message="Введите Email"),
+                                   Email(message="Введите корректный Email")])
+    password = PasswordField('Пароль', validators=[DataRequired(message="Введите пароль"),
+                                                   InputRequired(message="Введите пароль"),
+                                                   Length(min=7, message="Минимальная длинна пароля 7 символов")])
--- forms/signup_form.py
+++ forms/signup_form.py
@@ -1,10 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField
-from wtforms.validators import EqualTo, InputRequired
+from wtforms.validators import EqualTo, DataRequired, InputRequired, Length, Email
 
 
 class SignUpForm(FlaskForm):
-    email = EmailField('Email', validators=[InputRequired(message="Введите Email")])
+    email = EmailField('Email',
+                       validators=[DataRequired(message="Введите Email"), InputRequired(message="Введите Email"),
+                                   Email(message="Введите корректный Email")])
     password = PasswordField('Пароль', validators=[InputRequired(message="Введите пароль"),
-                                                   EqualTo('repeat_password', message="Пароли не совпадают")])
-    repeat_password = PasswordField('Повторите пароль', validators=[InputRequired(message="Введите пароль еще раз")])
+                                                   EqualTo('repeat_password', message="Пароли не совпадают"),
+                                                   Length(min=7, message="Минимальная длинна пароля 7 символов")])
+    repeat_password = PasswordField('Повторите пароль', validators=[InputRequired(message="Введите пароль еще раз"),
+                                                                    Length(min=7,
+                                                                           message="Минимальная длинна пароля 7 символов")])
